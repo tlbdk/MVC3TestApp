0fd8a075fc202c6443ae9c1f8e5106c12dc069d2
Troels Liebe Bentsen <troels@it-kartellet.dk>
2012-04-05 22:38:37 +0200

    Create simple test project the run a selendium test with IE

---
+++ TestApp.Tests/Selendium/MainSeleniumTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace TestApp.Tests.Controllers
+{
+
+    [TestFixture]
+    class MainSeleniumTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+	    public void Setup()
+	    {
+            // Link to fix this driver:  http://www.tomdupont.net/2011/10/use-internetexplorerdriver-for.html
+		    driver = new InternetExplorerDriver();
+	    }
+	    
+	    [TearDown]
+	    public void Teardown()
+	    {
+		    driver.Quit();
+	    }
+
+        [Test]
+        public void TestSearchGoogleForTheAutomatedTester()
+        {
+            //Navigate to the site
+            driver.Navigate().GoToUrl("http://www.google.co.uk");
+            //Find the Element and create an object so we can use it
+            IWebElement queryBox = driver.FindElement(By.Name("q"));
+            //Work with the Element that's on the page
+            queryBox.SendKeys("The Automated Tester");
+            queryBox.SendKeys(Keys.ArrowDown);
+            queryBox.Submit();
+            Console.WriteLine(driver.Title);
+            //Check that the Title is what we are expecting
+            Assert.True(driver.Title.IndexOf("Google") > -1);
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ TestApp.Tests/TestSuite.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace TestRunner
+{
+    class TestSuite
+    {
+        static void Main(string[] args)
+        {
+            int nTests = 0;
+            int nFailedTests = 0;
+
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (t.GetCustomAttributes(typeof(TestFixtureAttribute), false).Length != 0)
+                {
+                    // Gather test info.
+                    MethodInfo mFixtureSetup = null;
+                    MethodInfo mFixtureTearDown = null;
+                    MethodInfo mSetup = null;
+                    MethodInfo mTearDown = null;
+                    List<MethodInfo> mTests = new List<MethodInfo>();
+                    foreach (MethodInfo m in t.GetMethods())
+                    {
+                        if (m.GetCustomAttributes(typeof(TestFixtureSetUpAttribute), false).Length != 0)
+                            mFixtureSetup = m;
+
+                        if (m.GetCustomAttributes(typeof(TestFixtureTearDownAttribute), false).Length != 0)
+                            mFixtureTearDown = m;
+
+                        if (m.GetCustomAttributes(typeof(SetUpAttribute), false).Length != 0)
+                            mSetup = m;
+
+                        if (m.GetCustomAttributes(typeof(TearDownAttribute), false).Length != 0)
+                            mTearDown = m;
+
+                        if (m.GetCustomAttributes(typeof(TestAttribute), false).Length != 0)
+                            mTests.Add(m);
+                    }
+
+                    // Run tests
+                    object obj = Activator.CreateInstance(t);
+
+                    if (mFixtureSetup != null)
+                        mFixtureSetup.Invoke(obj, null);
+
+                    foreach (MethodInfo m in mTests)
+                    {
+                        nTests++;
+                        if (mSetup != null)
+                            mSetup.Invoke(obj, null);
+
+                        try
+                        {
+                            m.Invoke(obj, null);
+                        }
+                        catch (Exception)
+                        {
+                            nFailedTests++;
+                            Console.WriteLine(String.Format("Exception thrown in {0} during {1} test. ", t.Name, m.Name));
+                        }
+
+                        if (mTearDown != null)
+                            mTearDown.Invoke(obj, null);
+                    }
+
+                    if (mFixtureTearDown != null)
+                        mFixtureTearDown.Invoke(obj, null);
+                }
+            }
+
+            Console.WriteLine("{0} tests. {1} failed", nTests, nFailedTests, "Unit test results");
+            Console.ReadKey();
+        }
+
+    }
+}
